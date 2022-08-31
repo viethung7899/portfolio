b48068b08964a7c64abe1ed9e2df83a4935d248d
Viet Hung Nguyen <vhn@sfu.ca>
2022-08-30 20:49:22 -0700

    Apply dark mode

---
+++ src/layouts/MobileMenu.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { slide } from 'svelte/transition';
+  let show = false;
+</script>
+
+<button
+  class="toggle block sm:hidden cursor-pointer"
+  on:click={() => {
+    show = !show;
+  }}
+>
+  <i class="fa-solid fa-bars hover:text-orange-400 hover:dark:text-teal-500" />
+</button>
+{#if show}
+  <nav
+    class="absolute w-full flex items-start flex-col gap-4 p-4 top-full left-0 sm:hidden bg-white dark:bg-gray-800 border-b-2"
+    transition:slide
+  >
+    <a
+      href="/"
+      class="font-bold hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
+      >Home</a
+    >
+    <a
+      href="/projects"
+      class="font-bold hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
+      >Projects</a
+    >
+    <a
+      href="/blogs"
+      class="font-bold hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
+      >Blogs</a
+    >
+    <a
+      href="/contact"
+      class="font-bold hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
+      >Contact</a
+    >
+  </nav>
+{/if}
+
---
+++ src/layouts/ThemeSwitcher.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from 'svelte';
+
+  $: theme = localStorage.getItem('theme') || 'light';
+  let mounted = false;
+
+  onMount(() => {
+    mounted = true;
+  });
+
+  $: {
+    if (mounted) {
+      if (theme === 'dark') {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
+      localStorage.setItem('theme', theme);
+    }
+  }
+</script>
+
+{#if mounted}
+  <button on:click={() => (theme = theme === 'light' ? 'dark' : 'light')}>
+    <i class="fa-solid fa-sun" />
+  </button>
+{/if}
