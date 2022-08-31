29d0382b783088858651268bf12cb52f880e746d
Viet Hung Nguyen <vhn@sfu.ca>
2022-08-31 11:08:09 -0700

    Add new component

---
+++ src/layouts/Link.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let path: string;
+  export let title: string;
+  export let isMobile = false;
+</script>
+
+<a
+  href={path}
+  class={`
+    font-bold
+    hover:text-transparent 
+    hover:bg-clip-text 
+    hover:bg-gradient-to-r 
+    hover:from-red-600 
+    hover:to-yellow-600 
+    hover:dark:from-blue-400 
+    hover:dark:to-green-400
+    ${isMobile ? "" : "hidden sm:block"}`}
+  >{title}</a
+>
--- src/layouts/MobileMenu.svelte
+++ src/layouts/MobileMenu.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { slide } from 'svelte/transition';
+  import Link from "./Link.svelte"
   let show = false;
 </script>
 
@@ -11,35 +12,8 @@
     class="absolute w-full flex items-start flex-col gap-4 p-4 top-full left-0 sm:hidden bg-white dark:bg-gray-800 border-b-2"
     transition:slide
   >
-    <a
-      href="/"
-      class="hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
-      >Home</a
-    >
-    <a
-      href="/projects"
-      class="hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
-      >Projects</a
-    >
-    <a
-      href="/blogs"
-      class="hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
-      >Blogs</a
-    >
-    <a
-      href="/contact"
-      class="hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
-      >Contact</a
-    >
+  <Link path="/" title="Home" isMobile />
+  <Link path="/projects" title="Projects" isMobile />
+  <Link path="/blogs" title="Blogs" isMobile />
   </nav>
 {/if}
-
-<style>
-  a {
-    @apply font-bold;
-  }
-
-  a:hover {
-    @apply text-transparent bg-clip-text bg-gradient-to-r;
-  }
-</style>
