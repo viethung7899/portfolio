67f64927cb35a72c6489a2933032edc50ae92189
Viet Hung Nguyen <vhn@sfu.ca>
2022-08-30 21:40:03 -0700

    Finish layout

--- src/layouts/MobileMenu.svelte
+++ src/layouts/MobileMenu.svelte
@@ -3,12 +3,7 @@
   let show = false;
 </script>
 
-<button
-  class="toggle block sm:hidden cursor-pointer"
-  on:click={() => {
-    show = !show;
-  }}
->
+<button class="block sm:hidden cursor-pointer" on:click={() => (show = !show)}>
   <i class="fa-solid fa-bars hover:text-orange-400 hover:dark:text-teal-500" />
 </button>
 {#if show}
@@ -18,24 +13,33 @@
   >
     <a
       href="/"
-      class="font-bold hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
+      class="hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
       >Home</a
     >
     <a
       href="/projects"
-      class="font-bold hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
+      class="hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
       >Projects</a
     >
     <a
       href="/blogs"
-      class="font-bold hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
+      class="hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
       >Blogs</a
     >
     <a
       href="/contact"
-      class="font-bold hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
+      class="hover:from-red-500 hover:to-yellow-500 hover:dark:from-blue-500 hover:dark:to-green-500"
       >Contact</a
     >
   </nav>
 {/if}
 
+<style>
+  a {
+    @apply font-bold;
+  }
+
+  a:hover {
+    @apply text-transparent bg-clip-text bg-gradient-to-r;
+  }
+</style>
--- src/layouts/ThemeSwitcher.svelte
+++ src/layouts/ThemeSwitcher.svelte
@@ -2,7 +2,7 @@
   import { onMount } from 'svelte';
 
   let theme = localStorage.getItem('theme') || 'light';
-  $: isDark = theme === "dark";
+  $: isDark = theme === 'dark';
   let mounted = false;
 
   onMount(() => {
@@ -23,12 +23,12 @@
 
 {#if mounted}
   {#if isDark}
-  <button on:click={() => theme = "light"}>
-    <i class="fas fa-moon hover:text-teal-500" />
-  </button>
+    <button on:click={() => (theme = 'light')}>
+      <i class="fas fa-moon hover:text-teal-500" />
+    </button>
   {:else}
-  <button on:click={() => theme = "dark"}>
-    <i class="fas fa-sun hover:text-orange-500" />
-  </button>
+    <button on:click={() => (theme = 'dark')}>
+      <i class="fas fa-sun hover:text-orange-500" />
+    </button>
   {/if}
 {/if}
