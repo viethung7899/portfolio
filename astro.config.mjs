cff325b863eb5d5eb8887cefc6e6c2f9197b4cc8
Viet Hung Nguyen <vhn@sfu.ca>
2022-09-01 11:40:55 -0700

    Enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), mdx(), react()],
+  output: 'server',
+  adapter: vercel(),
 });
