6155f3bd9526af1d0bd3d48fc379bf5192000149
Viet Hung Nguyen <vhn@sfu.ca>
2022-09-06 01:12:59 -0700

    Enable smooth scrolling

--- src/components/blogs/TableOfContent.svelte
+++ src/components/blogs/TableOfContent.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import type { MarkdownHeading } from 'astro';
+  let scrollY: number;
 
   export let headings: MarkdownHeading[];
+  const scrollTo = (href: string) => {
+    const e = document.querySelector(href);
+      e?.scrollIntoView({
+        behavior: 'smooth',
+      });
+  }
 </script>
 
+<svelte:window bind:scrollY={scrollY} />
 <h3 class="uppercase font-bold opacity-50">On this page</h3>
 {#each headings as heading}
   {@const styles = heading.depth <= 2 ? 'text-md' : 'text-sm ml-4'}
@@ -11,20 +19,12 @@
   <a
     {href}
     class={`${styles} hover:text-orange-500 hover:dark:text-teal-500`}
-    on:click|preventDefault={() => {
-      const e = document.querySelector(href);
-      e?.scrollIntoView({
-        behavior: 'smooth',
-      });
-    }}>{heading.text}</a
-  >
+  >{heading.text}</a>
 {/each}
 <p
   class="text-xs opacity-50 pt-4 cursor-pointer hover:text-orange-500 hover:dark:text-teal-500"
   on:click|preventDefault={() => {
-    document.body.scrollIntoView({
-      behavior: 'smooth',
-    });
+    document.body.scrollIntoView();
   }}
 >
   Back to top
