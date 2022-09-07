612c0634153edbd2679e021282fb121336218648
Viet Hung Nguyen <vhn@sfu.ca>
2022-09-06 22:10:37 -0700

    Add anchor link for headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
