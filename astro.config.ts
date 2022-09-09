f599615cd3b04e1d1e6a6d9a1f0e69befa749bb5
Viet Hung Nguyen <vhn@sfu.ca>
2022-09-09 14:04:59 -0700

    Use shiki

--- astro.config.ts
+++ astro.config.ts
@@ -5,13 +5,18 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import image from "@astrojs/image";
+import rehypePrettyCode from 'rehype-pretty-code';
+import { rehypePrettyCodeOptions } from './plugins/rehypePrettyCode';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   markdown: {
-    syntaxHighlight: "prism"
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [rehypePrettyCode, rehypePrettyCodeOptions]
+    ]
   },
   integrations: [tailwind(), svelte(), mdx(), react(), image()],
   vite: {
---
+++ plugins/rehypePrettyCode.ts
@@ -0,0 +1,9 @@
+import nightOwlDark from "./theme/night-owl-dark.json";
+import nightOwlLight from "./theme/night-owl-light.json";
+
+export const rehypePrettyCodeOptions = {
+  theme: {
+    dark: nightOwlDark,
+    light: nightOwlLight
+  }
+}
