06b3a7e102d4dcdf39c9120cf0d4ee5b09ad713e
Viet Hung Nguyen <vhn@sfu.ca>
2022-09-09 10:02:11 -0700

    Use Prism syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,27 +5,15 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import image from "@astrojs/image";
-import rehypePrettyCode from 'rehype-pretty-code';
-
-const rehypePrettyCodeOptions = {
-  theme: {
-    dark: "one-dark-pro",
-    light: "github-light",
-  },
-}
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   markdown: {
-    syntaxHighlight: false
+    syntaxHighlight: "prism"
   },
-  integrations: [tailwind(), svelte(), mdx({
-    rehypePlugins: [
-      [rehypePrettyCode, rehypePrettyCodeOptions]
-    ],
-  }), react(), image()],
+  integrations: [tailwind(), svelte(), mdx(), react(), image()],
   vite: {
     ssr: {
       external: ["svgo"]
