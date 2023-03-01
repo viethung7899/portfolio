13a605dd763aaab1f7db5bcc886ea1e8d192f202
Viet Hung Nguyen <vhn@sfu.ca>
2023-02-28 21:08:44 -0800

    Custom code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,43 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
+import rehypePrettyCode from 'rehype-pretty-code';
+
+const prettyCodeOptions = {
+  theme: 'one-dark-pro',
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [
+        {
+          type: 'text',
+          value: ' ',
+        },
+      ];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push('highlighted');
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ['word'];
+  },
+  tokensMap: {},
+  keepBackground: false,
+};
+
 export default defineConfig({
   integrations: [
-    mdx(),
+    mdx({
+      rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+      syntaxHighlight: false,
+      smartypants: true,
+      gfm: true,
+    }),
     svelte(),
     tailwind({
       config: {
