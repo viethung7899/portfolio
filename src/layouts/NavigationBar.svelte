7a4f148a2b2ddf18bbc5d73603507232fb5a8d0b
Viet Hung Nguyen <vhn@sfu.ca>
2023-03-05 16:24:40 -0800

    Add some color

--- src/layouts/NavigationBar.svelte
+++ src/layouts/NavigationBar.svelte
@@ -21,7 +21,7 @@
   ];
 </script>
 
-<nav class="sticky top-0 backdrop-blur-lg flex flex-col items-center border-b-2 border-neutral z-50">
+<nav class="sticky top-0 backdrop-blur-lg bg-base-200 flex flex-col items-center z-50">
   <div class="flex flex-row items-center justify-between p-4 w-full max-w-screen-lg">
     <button
       class="block sm:hidden"
