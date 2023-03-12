da675f4e53df3c32886f641c0b8e182d099e40a2
Viet Hung Nguyen <vhn@sfu.ca>
2023-03-11 22:43:41 -0800

    Use input for the square

--- src/interactives/sodoku/SodokuSolver.svelte
+++ src/interactives/sodoku/SodokuSolver.svelte
@@ -1,35 +1,20 @@
 <script lang="ts">
   import Square from "./components/Square.svelte";
-  import Sodoku, { equal, type CellPosition } from "./lib/sodoku";
-  const INVALID_POSITION = [-1, -1, -1] as CellPosition;
+  import Sodoku from "./lib/sodoku";
 
   let sodoku = new Sodoku();
   $: board = sodoku.board;
-  let selected: CellPosition = INVALID_POSITION;
-
-  $: isSelected = !equal(selected, INVALID_POSITION);
-  $: isValidSodoku = sodoku.isValid();
-
-  const onKeyPress = (ev: KeyboardEvent) => {
-    if (!isSelected) return;
-    const number = +ev.key;
-    if (Number.isNaN(number)) return;
-    sodoku = sodoku.setValue(selected, number);
-  }
 </script>
 
-<svelte:window on:keypress={onKeyPress} />
 <div class="w-full flex">
   <div class="inline-grid grid-cols-3 border-2 border-slate-500 m-auto">
     {#each board as region, regionIndex}
       <div class="inline-grid grid-cols-3 border-2 border-slate-500">
         {#each region as row, rowIndex}
           {#each row as square, colIndex}
-            <Square
-              value={square}
-              highlight={equal(selected, [regionIndex, rowIndex, colIndex])}
-              onClick={() => (selected = [regionIndex, rowIndex, colIndex])}
-            />
+            <Square value={square} onChange={(value) => {
+              sodoku = sodoku.setValue([regionIndex, rowIndex, colIndex], value);
+            }} />
           {/each}
         {/each}
       </div>
@@ -37,12 +22,10 @@
   </div>
 </div>
 <div class="flex my-6 items-center justify-center gap-3">
-  <button 
-    class="btn" 
-    disabled={equal(selected, INVALID_POSITION)}
-    on:click={() => selected = INVALID_POSITION}>
-    Deselect
-  </button>
+  <select class="select select-bordered">
+    <option>Backtracking</option>
+    <option>Dacing Link</option>
+  </select>
   <button class="btn btn-primary btn-outline" on:click={() => sodoku = sodoku.randomize()}>
     Randomize
   </button>
--- src/interactives/sodoku/components/Square.svelte
+++ src/interactives/sodoku/components/Square.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
   export let value: number;
-  export let highlight: boolean;
-  export let onClick: () => void;
+  export let onChange: (value: number) => void;
 </script>
 
-<div
-  class={`border border-slate-500 ${highlight ? "bg-secondary/50" : ""}
-  w-12 h-12 p-2 
-  flex items-center justify-center
-  cursor-pointer text-xl`}
-  on:click={onClick}
-  on:keydown={undefined}
->
-  {value > 0 && value < 10 ? value : ""}
-</div>
+<input
+  class={`border border-slate-500 bg-transparent
+  w-12 h-12 p-2 cursor-pointer text-xl 
+  focus:outline text-center
+  focus:bg-primary/30`}
+  type="number"
+  value={value > 0 ? value : ""}
+  min="0"
+  max="9"
+  on:input={(ev) => {
+    value = ev.currentTarget.valueAsNumber;
+    if (value > 9) {
+      value = 9;
+    };
+    onChange(value);
+  }}
+/>
+
+<style>
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type="number"] {
+    -moz-appearance: textfield;
+  }
+</style>
